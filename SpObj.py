c0ff1a2c5b2ad857d658e8b5800bf17d992fb144
Edo-Hachi <yukikaze1564@gmail.com>
2022-06-13 22:54:54 +0900

    bug fix SpObj.py

--- SpObj.py
+++ SpObj.py
@@ -2,62 +2,66 @@ import pyxel
 
 class sprite:
     def __init__(self):
-        self.__x = -100      #スプライト表示座標
-        self.__y = -100
+        self._x = -100      #スプライト表示座標
+        self._y = -100
 
-        self.__w = 16    #スプライトサイズ  
-        self.__h = 16
+        self._w = 16    #スプライトサイズ  
+        self._h = 16
 
-        self.__ox = self.__w / 2    #原点
-        self.__oy = self.__h / 2
+        self._ox = self._w / 2    #原点
+        self._oy = self._h / 2
         
-        self.__col_r = -1   #collision round size
-        self.__show = False
+        self._col_r = -1   #collision round size
+        self._show = False
 
-        self.__imgpage = 0 #スプライト参照ページ
+        self._imgpage = 0 #スプライト参照ページ
 
-        self.__imgu = 0 #参照座標
-        self.__imgv = 0
+        self._imgu = 0 #参照座標
+        self._imgv = 0
 
-        self.__imgc = 15  #マスクカラー
+        self._imgc = 15  #マスクカラー
 
     #スプライト情報設定
     def spset(self, imgpage, w,h, ox, oy, u, v, mask_color):
-        self.__imgpage = imgpage
+        self._imgpage = imgpage
 
-        self.__w = w    #スプライトサイズ  
-        self.__h = h
+        self._w = w    #スプライトサイズ  
+        self._h = h
 
-        self.__ox = ox*-1  #原点
-        self.__oy = oy*-1        
+        self._ox = ox*-1  #原点
+        self._oy = oy*-1        
 
-        self.__imgu = u #リソース画像の参照座標　
-        self.__imgv = v
+        self._imgu = u #リソース画像の参照座標
+        self._imgv = v
         
-        self.__imgc = mask_color  #マスクカラー
+        self._imgc = mask_color  #マスクカラー
 
     #原点設定
     def sphome(self, ox, oy):
-        self.__ox = ox*-1    #原点
-        self.__oy = oy*-1
+        self._ox = ox*-1    #原点
+        self._oy = oy*-1
     
     def spshow(self, show:bool):
-        self.__show = show
+        self._show = show
 
     #スプライト描画
     def spdraw(self, x, y):
-        self.__x = x
-        self.__y = y
-        if self.__show == True:
-            pyxel.blt(self.__x+self.__ox, self.__y+self.__oy, self.__imgpage, self.__imgu, self.__imgv, self.__w, self.__h, self.__imgc)
+        self._x = x
+        self._y = y
+        if self._show == True:
+            pyxel.blt(self._x+self._ox, self._y+self._oy, self._imgpage, self._imgu, self._imgv, self._w, self._h, self._imgc)
             #pyxel.blt(self.__x, self.__y, self.__imgpage, self.__imgu, self.__imgv, self.__w, self.__h, self.__imgc)
     
-    #コリジョン円半径
+    #コリジョン円[半径]
     def spcolc(self, r):
-        self.__col_r = r
+        self._col_r = r
     
     #コリジョン範囲表示
-    def show_collision_r(self):
-        if self.__col_r != -1:
-            pyxel.circb(self.__x, self.__y, self.__col_r, 8)
+    def show_collision_r(self, fill=0):
+        if self._col_r != -1:
+            if fill==0:
+                pyxel.circb(self._x, self._y, self._col_r, 8)
+            else:
+                pyxel.circ(self._x, self._y, self._col_r, 8)
+
 
--- pyAction.py
+++ pyAction.py
@@ -8,11 +8,11 @@ FPS=60
 
 class App:
 
+    #床描画
     def drwflr(self):
         x=0
         for x in range(0,WIDTH,16):
             pyxel.blt(x,HEIGHT-16, 1, 0,0, 16,16)
-    
 
 
     def __init__(self):
@@ -22,18 +22,23 @@ class App:
         #self.plsp = SpObj.sprite(100,100,0,0,True)
         #self.plsp.spset(0, 0,0, 15)
 
+        self.plsp = SpObj.sprite()
+        self.plsp.spset(0, 16,16, 8,8, 0,0, 15)
+        self.plsp.spshow(True)
+        self.plsp.spcolc(8)
+
         self.ensp = SpObj.sprite()
         self.ensp.spset(0, 16,16, 8,8, 16,0, 15)
         self.ensp.spshow(True)
         self.ensp.spcolc(8)
 
-        self.x1=100
-        self.y1=100
-        self.r1=25
+        #self.x1=100
+        #self.y1=100
+        #self.r1=25
 
-        self.x2=130
-        self.y2=130
-        self.r2=15
+        #self.x2=130
+        #self.y2=130
+        #self.r2=32
 
         self.ex=WIDTH   #enemy pos
         self.ey=HEIGHT-32
@@ -46,8 +51,8 @@ class App:
         if pyxel.btnp(pyxel.KEY_Q):
             pyxel.quit()
         
-        self.x2 = pyxel.mouse_x
-        self.y2 = pyxel.mouse_y
+        self.px = pyxel.mouse_x
+        self.py = pyxel.mouse_y
 
         self.ex -=4
         if self.ex<-16:
@@ -60,11 +65,14 @@ class App:
         #床
         self.drwflr()
 
+        #PlayerChar
+        self.plsp.spdraw(self.px, self.py)
+        self.plsp.show_collision_r(0)
+
         #障害物
         #pyxel.blt(self.ex,self.ey, 0, 16,0, 16, 16)
         self.ensp.spdraw(200, 200)
-        self.ensp.show_collision_r()
-
+        self.ensp.show_collision_r(0)
 
         #円形コリジョンの衝突判定
         #self.dist = pyxel.sqrt((self.x2-self.x1)**2 + (self.y2-self.y1)**2)
@@ -72,12 +80,19 @@ class App:
         #if self.dist < (self.r1 + self.r2):
         #    self.color= 8
 
-        #pyxel.circ(self.x1, self.y1, self.r1, self.color)
-        #pyxel.circ(self.x2, self.y2, self.r2, 6)
+        #self.dist = pyxel.sqrt((self.x2-(self.ensp._x + self.ensp._ox))**2 + (self.y2-(self.ensp._y+self.ensp._oy))**2)
+        dist = pyxel.sqrt((self.plsp._x-self.ensp._x)**2 + (self.plsp._y-self.ensp._y)**2)
+        #self.color = 6
+        
+        if dist < self.plsp._col_r + self.ensp._col_r:
+        #if self.dist <= (self.ensp._col_r):
+            #self.color = 8
+            self.plsp.show_collision_r(1)
 
-        #pyxel.text(10,10,str(round(self.dist,2)),7)
+        pyxel.text(10,10,str(round(dist,2)),7)
 
-        pyxel.line(0,0, 200,200,9)
+        #pyxel.line(0,0, self.px, self.py,9)
 
 
 App()
+
