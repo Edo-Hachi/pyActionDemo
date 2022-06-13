3ab40497acb069802e33119e92050b499c8f1047
Edo-Hachi <yukikaze1564@gmail.com>
2022-06-13 12:53:05 +0900

    Add floor and enemy

--- SpObj.py
+++ SpObj.py
@@ -1,42 +1,32 @@
 import pyxel
 
 class sprite:
-    #self.Enable = True
-
     def __init__(self, x, y, ox, oy, alive):
-
         self.__x = x      #スプライト表示座標
         self.__y = y
-        #self.__w = w      #スプライト幅、高さ
-        #self.__h = h
         self.__ox = ox    #原点
         self.__oy = oy
-
+        self.__r = 16   #collision round size
         self.__alive = alive
 
-    @property
-    def alive(self):
-        return self.__alive
-    
-    #@property
-    #def sphome(self):
-    #    return self.__ox,self.__oy
-
-    @alive.setter
-    def alive(self, val):
-        self.__alive = val
-
+    #スプライト情報設定
     def spset(self, img, u, v, col):
         self.__img = img
         self.__imgu = u
         self.__imgv = v
         self.__imgc = col  #マスクカラー
 
+    #原点設定
     def sphome(self, ox, oy):
         self.__ox = ox    #原点
         self.__oy = oy
-   
+    
+    #スプライト描画
     def spdraw(self, x, y, w, h):
         if self.__alive == True:
             pyxel.blt(self.__x + self.__ox, self.__y+self.__oy, self.__img, self.__imgu, self.__imgv, w, h, self.__imgc)
     
+    #コリジョン円直径
+    def spcolc(self, r):
+        self.__r = r
+
--- pyAction.py
+++ pyAction.py
@@ -6,6 +6,14 @@ HEIGHT=512
 FPS=60
 
 class App:
+
+    def drwflr(self):
+        x=0
+        for x in range(0,WIDTH,16):
+            pyxel.blt(x,HEIGHT-16, 1, 0,0, 16,16)
+    
+
+
     def __init__(self):
         self.newsp = SpObj.sprite(10,10,-8,-8,True)
         self.newsp.spset(0,0,0,15)
@@ -18,6 +26,8 @@ class App:
         self.y2=130
         self.r2=15
 
+        self.ex=WIDTH   #enemy pos
+        self.ey=HEIGHT-32
 
         pyxel.init(WIDTH, HEIGHT, "pyActionTest", FPS)
         pyxel.load("./assets/pyActTest.pyxres")
@@ -30,6 +40,10 @@ class App:
         self.x2 = pyxel.mouse_x
         self.y2 = pyxel.mouse_y
 
+        self.ex -=4
+        if self.ex<-16:
+            self.ex=WIDTH
+
         #self.dist = pyxel.sqrt((self.x2-self.x1)**2 + (self.y2-self.y1)**2)
 
         #pass
@@ -39,13 +53,17 @@ class App:
         #pyxel.text(55, 41, "Hello, Pyxel!", pyxel.frame_count % 16)
         #pyxel.blt(61, 66, 0, 0, 0, 38, 16)
 
-        self.dist = pyxel.sqrt((self.x2-self.x1)**2 + (self.y2-self.y1)**2)
+        pyxel.blt(100,100, 0, 0, 0, 16, 16)
+        self.drwflr()
 
-         
+        pyxel.blt(self.ex,self.ey, 0, 16,0, 16, 16)
+
+
+        #円形コリジョンの衝突判定
+        self.dist = pyxel.sqrt((self.x2-self.x1)**2 + (self.y2-self.y1)**2)
+        self.color = 6
         if self.dist < (self.r1 + self.r2):
             self.color= 8
-        else:
-            self.color = 6
 
         pyxel.circ(self.x1, self.y1, self.r1, self.color)
         pyxel.circ(self.x2, self.y2, self.r2, 6)
@@ -54,9 +72,8 @@ class App:
 
         self.newsp.alive = True
         self.newsp.spdraw(10,10, -16, 16)
+        self.__r = 10
 
         pyxel.text(10,10,str(round(self.dist,2)),7)
-
-
 
 App()
