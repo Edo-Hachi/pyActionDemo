6e63e7f43cbe47385f854fb525eb809d86ae0e63
Edo-Hachi <yukikaze1564@gmail.com>
2022-06-15 15:20:28 +0900

    なんとかうごいてるけどきもちわるい

--- pyAction.py
+++ pyAction.py
@@ -16,7 +16,7 @@ class App:
         for x in range(0,WIDTH,16):
             flr = SpObj.sprite()
             flr.spset(1, 16,16, 0,0, 0,0, 15)
-            #flr.sphome(0,0)
+            flr.sphome(0,0)
             flr.spshow(True)
             flr.spcolr(0,0,16,16)
 
@@ -29,28 +29,28 @@ class App:
         #print(len(FloorList))
         for i in range(len(FloorList)):
             FloorList[i].spdraw(x, HEIGHT-16)
-            FloorList[i].show_collision_r(False)
+            #FloorList[i].show_collision_r(False)
             x+=16
 
     def __init__(self):
 
+        self.px=60
+        self.py=HEIGHT-32
+
+        self.vx = 0
+        self.vy = 0
+        self.initVy = -10
+        self.gravity = 1
+        self.flgJmp=False
 
         self.plsp = SpObj.sprite()
         self.plsp.spset(0, 16,16, 8,8, 0,0, 15)
-        self.plsp.sphome(8,8)
+        #self.plsp.sphome(8,8)
+        self.plsp.sphome(0,0)
         self.plsp.spshow(True)
         self.plsp.spcolc(8)
         self.plsp.spcolr(0,0,16,16)
 
-
-        self.ensp = SpObj.sprite()
-        self.ensp.spset(0, 16,16, 8,8, 16,0, 15)
-        self.ensp.spshow(True)
-        self.ensp.spcolc(8)
-        self.ensp.spcolr(0,0,16,16)
-
-        self.ex=WIDTH   #enemy pos
-        self.ey=HEIGHT-32
 
         self.initFlr()
 
@@ -61,40 +61,42 @@ class App:
     def update(self):
         if pyxel.btnp(pyxel.KEY_Q):
             pyxel.quit()
+
+        #右へ移動
+        self.px += self.vx
+
+        #Jump
+        if pyxel.btnp(pyxel.KEY_SPACE):
+            if self.flgJmp == False:
+                self.vy = self.initVy
+                self.flgJmp = True
         
-        self.px = pyxel.mouse_x
-        self.py = pyxel.mouse_y
+        if self.flgJmp == True:
+            self.py += self.vy
+            self.vy += self.gravity
 
-        self.ex -=4
-        if self.ex<-16:
-            self.ex=WIDTH
-
-
+        #床との接触判定
+        for i in range(len(FloorList)):
+            if self.plsp.sphitr(FloorList[i])==True:
+                #FloorList[i].show_collision_r(True)
+                self.flgJmp = False
+                self.vy=0
+                self.py=FloorList[i]._y-16
+                #print(FloorList[i]._y)
+                #print(self.py)
+                
     def draw(self):
         pyxel.cls(1)
 
-        #床
-        self.drwFlr()
 
         #PlayerChar
         self.plsp.spdraw(self.px, self.py)
         #self.plsp.show_collision_c(0)
-        self.plsp.show_collision_r(False)
+        #self.plsp.show_collision_r(False)
 
-        #障害物
-        self.ensp.spdraw(100, 100)
-        self.ensp.show_collision_r(False)
-        #self.ensp.show_collision_c(0)
-
-        if self.plsp.sphitr(self.ensp) == True:
-            self.ensp.show_collision_r(True)
-        
-        #床との接触判定
-        for i in range(len(FloorList)):
-            if self.plsp.sphitr(FloorList[i])==True:
-                #print("Hit")
-                FloorList[i].show_collision_r(True)
-                
+      #床
+        self.drwFlr()
+ 
 
         #pyxel.line(0,0, self.px, self.py,9)
 
